98382ac8e3ed6e53519a97c62c3792aff20b1a4d
shubhalip <90958916+shubhalip@users.noreply.github.com>
2021-09-18 21:02:40 +0530

    //initial commit of math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+//Add implementations here
+def add(x,y):
+    pass
+
+//subtract implementations here
+def subtract(x,y):
+    pass
+
+//multiply implementations here
+def multiply(x,y):
+    pass
+def divide(x,y):
+    pass
